4564351ab56a72f5487c3b0e41c2d5e6421e70db
Jamil Bousquet <jamil.bousquet@gmail.com>
2024-02-29 07:05:25 -0400

    📝 Add documentation to helpers

--- assignments/P02/headers/preamble.h
+++ assignments/P02/headers/preamble.h
@@ -4,10 +4,41 @@
 using namespace std;
 using json = nlohmann::json;
 
+/// @brief color format top 10 query results
+/// @param matches all matches from current query
+/// @param word word criteria of current query
 void colorResults(vector<string> & matches, string word);
+
+/// @brief print stats on the number of matches and duration of a query
+/// @param numberOfQueryResults number of matches from current query
+/// @param nanoseconds time duration for getting current query
 void printStats(int numberOfQueryResults, int nanoseconds);
-vector<string> getDictionaryMatches(json dictionary, string partialKey);
-pair<vector<string>, int> partialMatch(json dictionary, string substring);
+
+/// @brief Get a vector of matches from a given dictionary based on a key
+/// @param dictionary dictionary containing possible matches
+/// @param partialKey key criteria for retrieving matches
+/// @return all dictionary entries which fit the given key criteria
+
+/// @brief Finds partial matches in an array of strings
+/// @param words array to search
+/// @param substring substring to search for in each word
+/// @return all the matched words from substring
+
+/// @brief cache a portion of a given `dictionary` to the given cache
+/// @details cache is built from the first letter of the given `word`
+/// @param dictionary full dictionary `json` object
+/// @param dictionaryCache container for cached dictionary
+/// @param word word whose first letter is used as caching criteria
 void cacheDictionary(json & dictionary, json & dictionaryCache, string word);
+
+/// @brief Append `typed_char` or pop last letter in `word`
+/// @param typed_char either a alphanum or a backspace key
+/// @param word word currently in buffer
+/// @return boolean of whether or not an error occured
 bool buildWord(char typed_char, string & word);
+
+/// @brief Load .json data from a given file at `pathName`
+/// @param pathName absolute location of .json file
+/// @param _logger `Logger` object to log errors
+/// @return nlohmann `json` deserialization of `pathName`
 json loadData(string pathName, Logger<string> & _logger);
